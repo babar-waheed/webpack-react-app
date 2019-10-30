70a317de9906906d964e44ffc94db1eca96b56c4
Babar Waheed <babar.waheed@gmail.com>
2019-10-30 15:36:39 +1100

    Production check = NODE_EVN=production node server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackConfig = require('./webpack.config');
+
 
 const app = express();
-app.use(webpackMiddleware(webpack(webpackConfig)));
+
+
+console.log(process.env.NODE_EVN);
+
+if(process.env.NODE_EVN !== 'production'){
+    const webpack = require('webpack');
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpackConfig = require('./webpack.config');
+    app.use(webpackMiddleware(webpack(webpackConfig)));
+}else{
+    app.use(express.static('dist'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist/index.html'))
+    })
+}
 
 app.listen(3050, ()=> console.log('Listening'));
