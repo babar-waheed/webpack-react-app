9aa1548397f1e2019d8ce97bbcb07823caa9783a
Babar Waheed <babar.waheed@gmail.com>
2019-10-30 12:22:12 +1100

    code splitting and optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 let webpack = require('webpack');
 let path = require('path');
 
+const VENDOR_LIBS = [
+  'react',
+  'lodash',
+  'redux',
+  'react-redux',
+  'react-dom',
+  'faker',
+  'react-input-range',
+  'redux-form',
+  'redux-thunk'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
@@ -24,5 +39,10 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    })
+  ]
 };
